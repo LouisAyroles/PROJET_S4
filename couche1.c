4701d91c39499a6ad07dd1f048be33147776bbb4
csl0779a <leo.cusseau@univ-tlse3.fr>
2019-01-18 13:50:29 +0100

    Add des fichiers du prof (format disks+def.h) et de l'ebauche du fichier couche 1.

---
+++ couche1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void init_disk_raid5(virtual_disk_t *diskRaid)
+
+int compute_nblock(int n)
+
+void write_block(virtual_disk_t RAID5, block_t entrant, uint pos, int idDisk)
+
+///\brief renvoie un code d'erreur si echec de lecture
+int read_block(virtual_disk_t RAID5, block_t *recup, uint pos, int idDisk)
+
+///\brief utiliser "fseek", "fwrite" et "fread"
+void block_repair(virtual_disk_t RAID5, uint pos, int idDisk)
+
+void affichageBlockHexa(block_t donnees, FILE *output)
+
