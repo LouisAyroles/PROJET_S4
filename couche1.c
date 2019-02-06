c9147aca363ecf793dbf7a1c8b79d82fdfe07053
csl0779a <leo.cusseau@univ-tlse3.fr>
2019-02-06 17:40:57 +0100

    ajout clarte brillance coquin

--- couche1.c
+++ couche1.c
@@ -5,17 +5,37 @@
 #include <fts.h>
 #include <errno.h>
 
+/**
+ * \file couche1.c
+ * \brief Programme couche1 du raid5.
+ * \author Groupe14
+ * \version 0.1
+ * \date 6 fevrier 2019
+ *
+ * Programme de la couche 1 du raid5.
+ *
+ */
+
 //Global raid disk variable
 virtual_disk_t *r5Disk;
 
-void init_disk_raid5(const char * repertoire){
-    size_t lengthRep = strlen(repertoire);
-    char *nomDisque = malloc(sizeof(char)*lengthRep+10);  //Creation d'une chaine pouvant contenir [repertoire]+10 caracteres
+/**
+
+@param void
+@return void
+**/
+void add_finChemin(const char * repertoire, char * nomDisque, size_t lengthRep){
     strcpy(nomDisque, repertoire);
     nomDisque[lengthRep] = '/';
     nomDisque[lengthRep+1] = 'd';
     nomDisque[lengthRep+2] = '0';
     nomDisque[lengthRep+3] = '\0';
+}
+
+void init_disk_raid5(const char * repertoire){
+    size_t lengthRep = strlen(repertoire);
+    char *nomDisque = malloc(sizeof(char)*lengthRep+10);  //Creation d'une chaine pouvant contenir [repertoire]+10 caracteres
+    add_finChemin(repertoire, nomDisque, lengthRep);
     r5Disk=malloc(sizeof(virtual_disk_t));
     r5Disk->ndisk=4;
     r5Disk->raidmode=CINQ;
@@ -29,11 +49,7 @@ void init_disk_raid5(const char * repertoire){
 void turn_off_disk_raid5(const char * repertoire){
     size_t lengthRep = strlen(repertoire);
     char *nomDisque = malloc(sizeof(char)*lengthRep+10);
-    strcpy(nomDisque, repertoire);
-    nomDisque[lengthRep] = '/';
-    nomDisque[lengthRep+1] = 'd';
-    nomDisque[lengthRep+2] = '0';
-    nomDisque[lengthRep+3] = '\0';
+    add_finChemin(repertoire, nomDisque, lengthRep);
     for (int i = 0; i < r5Disk->ndisk; i++){
         nomDisque[lengthRep+2] = i + '0';
         r5Disk->storage[i]=fclose(nomDisque);
@@ -76,5 +92,6 @@ void affichageBlockHexa(block_t donnees, FILE *output);
 int main(void){
 	init_disk_raid5("./RAIDFILES");
 	info_disque();
+  turn_off_disk_raid5("./RAIDFILES");
 	exit(0);
 }
