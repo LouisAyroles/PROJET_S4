ac36ddfeea4e78a28e7aaf12b0121b5a25a69bb1
csl0779a <leo.cusseau@univ-tlse3.fr>
2019-02-06 20:03:30 +0100

    debug read/write

--- couche1.h
+++ couche1.h
@@ -2,13 +2,14 @@
 #define COUCHE1_H
 
 void add_finChemin(const char * repertoire, char * nomDisque, size_t lengthRep);
-void init_disk_raid5(const char * repertoire);
-void turn_off_disk_raid5(const char * repertoire);
-void info_disque();
+void init_disk_raid5(const char * repertoire, virtual_disk_t *r5Disk);
+void turn_off_disk_raid5(const char * repertoire, virtual_disk_t *r5Disk);
+void info_disque(virtual_disk_t *r5Disk);
 int compute_nblock(int n);
-void write_block(virtual_disk_t *RAID5, block_t entrant, uint pos, int idDisk);
-int read_block(virtual_disk_t RAID5, block_t *recup, uint pos, int idDisk);
+void write_block(virtual_disk_t *RAID5, block_t *entrant, uint pos, int idDisk);
+int read_block(virtual_disk_t *RAID5, block_t *recup, uint pos, int idDisk);
 void block_repair(virtual_disk_t RAID5, uint pos, int idDisk);
-void affichageBlockHexa(block_t donnees, FILE *output);
+void bitToHexa(block_t monBloc, char* nbHexa);
+void affichageBlockHexa(virtual_disk_t *RAID5, int idDisk, uint pos, FILE *output);
 
 #endif
