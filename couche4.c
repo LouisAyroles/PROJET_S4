1717ec99dc4956ce30678f10e050f88058914e00
ImJustLouis <louis.ayroles@univ-tlse3.fr>
2019-04-02 13:21:33 +0200

    Squelette write file

--- couche4.c
+++ couche4.c
@@ -20,11 +20,42 @@
    * @param : virtual_disk_t
    * @param : chaine de char (nom du Fichier)
    * @param : file_t (contenu du fichier à ecrire)
-   * @return void
+   * @return int (1 si on a ecrit le fichier, 0 si plus de place ou echec)
  **/
- void write_file(virtual_disk_t *r5Disk, char *NomFichier, file_t fichier){
+ int write_file(virtual_disk_t *r5Disk, char *nomFichier, file_t fichier){
 
- }
+   int nbfiles = get_nb_files(r5Disk->inodes)
+   /*Il n'y a plus de places dans le systeme*/
+   if (nbfiles==10) {
+     return 0;
+   }
+  /*Est ce que le fichier est present dans le systeme?*/
+   while ( (nbfiles > 0) && (inodes[i]->filename != nomFichier) ) {
+     nbfiles--;
+   }
+   /*Le fichier n'est pas present dans le systeme*/
+   if (nbfiles == 0) {
+     uint start = r5Disk->super_block.first_free_byte;
+     int posInode = get_unused_inodes(r5Disk->inodes);
+     r5Disk->inodes[posInode]=init_inode(nomFichier, fichier->size, start);
+     //ecrire le fichier Ici
+     r5Disk->number_of_files+=1;
+
+   /*Le fichier est present dans le systeme*/
+   }else{
+     /*Le fichier est plus petit que le fichier present*/
+     if (fichier.size <= r5Disk->inodes[nbfiles].size) {
+       r5Disk->inodes[nbfiles].size = fichier.size;
+       //Mettre a jour les données
+
+     /*Le fichier est plus grand que le fichier present*/
+     }else{
+       delete_inode(r5Disk,nbfiles);
+       //identique au 1er if
+     }
+
+   }
+}
 
 
 
